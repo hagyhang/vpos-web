a69f0eaf8563254554b638049ad16014e00d6bb6
hagyhang <hagyhang@gmail.com>
2018-03-05 13:42:05 +0700

    5/3

--- js/chart.js
+++ js/chart.js
@@ -109,16 +109,3 @@ function toogleDataSeries(e){
     e.chart.render();
 }
 /*-- end options merchant --*/
-
-/*-- options datetimepicker --*/
-$(".form_date").datetimepicker({
-    //language:  'fr',
-    weekStart: 1,
-    todayBtn:  1,
-    autoclose: 1,
-    todayHighlight: 1,
-    startView: 2,
-    minView: 2,
-    forceParse: 0
-});
-/*-- end options datetimepicker --*/
